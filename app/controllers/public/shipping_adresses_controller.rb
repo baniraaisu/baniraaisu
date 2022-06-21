b9eb2e39a87663c1a874535bb4ceff419d1052f4
na08skt <sakata.na08@gmail.com>
2022-06-21 07:30:08 +0000

    preFinishCommit

--- app/controllers/public/shipping_adresses_controller.rb
+++ app/controllers/public/shipping_adresses_controller.rb
@@ -1,12 +1,33 @@
 class Public::ShippingAdressesController < ApplicationController
   def index
-    @shipping_addresses = ShippingAdress.new
+    @shipping_adress = ShippingAdress.new
+    @shipping_adresses = ShippingAdress.all
   end
-  
+
   def create
-    @shipping_addresses = ShippingAdress.new()
+    @shipping_adress = ShippingAdress.new(shipping_adress_params)
+    @shipping_adress.save
+    redirect_to request.referrer
   end
 
   def edit
+    @shipping_adress = ShippingAdress.find(params[:id])
+  end
+
+  def update
+    @shipping_adress = ShippingAdress.find(params[:id])
+    @shipping_adress.update(shipping_adress_params)
+    redirect_to public_shipping_adresses_path
+  end
+
+  def destroy
+    @shipping_adress = ShippingAdress.find(params[:id])
+    @shipping_adress.destroy
+    redirect_to request.referrer
+  end
+
+  private
+  def shipping_adress_params
+    params.require(:shipping_adress).permit(:post_code, :adress, :adress_name)
   end
 end
