b8c27f15859f508804e8174956a66f472061a6fd
a118skr <98032893+a118skr@users.noreply.github.com>
2022-06-23 03:39:04 +0000

    [Fix]enum支払い機能

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+
+enum status: {
+    credit_card: 0,
+    transfer: 1,
+}
+
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module Baniraaisu
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #enum日本語化
+    config.i18n.default_locale = :ja
+
   end
+
+
+
 end
