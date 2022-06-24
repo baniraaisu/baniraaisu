b43fde19fbf6b77d2525bcb1f0b55289a326740d
EC2 Default User <ec2-user@ip-172-31-80-47.ec2.internal>
2022-06-24 03:46:34 +0000

    second

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @customers = Customer.all
+    @customers = Customer.all.page(params[:page]).per(10)
   end
 
   def show
@@ -14,8 +14,11 @@ class Admin::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(params[:id])
-    @customer.update(customer_params)
-    redirect_to admin_customer_path
+    if @customer.update(customer_params)
+       redirect_to admin_customer_path
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,12 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :cart_items, dependent: :destroy
+  validates :first_name, :last_name, :adress, :email, presence: true
+  validates :tel_number, format: { with: /\A\d{10,11}\z/ }
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :post_code, format: { with: /\A\d{7}\z/ }
 
   def active_for_authentication?
     super && (self.is_deleted == false)
