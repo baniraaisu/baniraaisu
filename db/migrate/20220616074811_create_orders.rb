ad53e605bf6c5b2fd52dde82a672ca2ca4128e75
na08skt <sakata.na08@gmail.com>
2022-06-25 16:19:59 +0000

    BreakCommit

--- db/migrate/20220616074811_create_orders.rb
+++ db/migrate/20220616074811_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :customer_id
       t.integer :pay_method
       t.string :adress_name
       t.string :post_code
